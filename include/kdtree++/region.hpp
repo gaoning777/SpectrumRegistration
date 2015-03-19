56d1330179a355b53449d6d0bd8b74372040a648
Ning Gao <ning.gao@colorado.edu>
2015-03-19 17:11:13 -0600

    kd-tree structure analysis, identation added

--- include/kdtree++/region.hpp
+++ include/kdtree++/region.hpp
@@ -14,125 +14,107 @@
 namespace KDTree
 {
 
-  template <size_t const __K, typename _Val, typename _SubVal,
+   template <size_t const __K, typename _Val, typename _SubVal,
             typename _Acc, typename _Cmp>
-    struct _Region
-    {
-      typedef _Val value_type;
-      typedef _SubVal subvalue_type;
+               struct _Region
+               {
+                  typedef _Val value_type;
+                  //_SubVal is a distance type for a certain dimension
+                  typedef _SubVal subvalue_type;
 
-      // special typedef for checking against a fuzzy point (for find_nearest)
-      // Note the region (first) component is not supposed to have an area, its
-      // bounds should all be set to a specific point.
-      typedef std::pair<_Region,_SubVal> _CenterPt;
+                  // special typedef for checking against a fuzzy point (for find_nearest)
+                  // Note the region (first) component is not supposed to have an area, its
+                  // bounds should all be set to a specific point.
+                  //_CenterPt is a fuzzy region with _SubVal range
+                  typedef std::pair<_Region,_SubVal> _CenterPt;
 
-      _Region(_Acc const& __acc=_Acc(), const _Cmp& __cmp=_Cmp())
-	: _M_cmp(__cmp), _M_acc(__acc) {}
+                  _Region(_Acc const& __acc=_Acc(), const _Cmp& __cmp=_Cmp())
+                     : _M_cmp(__cmp), _M_acc(__acc) {}
 
-      template <typename Val>
-      _Region(Val const& __V,
-	      _Acc const& __acc=_Acc(), const _Cmp& __cmp=_Cmp())
-	: _M_acc(__acc), _M_cmp(__cmp)
-      {
-        for (size_t __i = 0; __i != __K; ++__i)
-          {
-             _M_low_bounds[__i] = _M_high_bounds[__i] = _M_acc(__V,__i);
-          }
-      }
+                  template <typename Val>
+                     _Region(Val const& __V,
+                           _Acc const& __acc=_Acc(), const _Cmp& __cmp=_Cmp())
+                     : _M_acc(__acc), _M_cmp(__cmp)
+                     {
+                        for (size_t __i = 0; __i != __K; ++__i)
+                        {
+                           _M_low_bounds[__i] = _M_high_bounds[__i] = _M_acc(__V,__i);
+                        }
+                     }
 
-      template <typename Val>
-      _Region(Val const& __V, subvalue_type const& __R,
-	      _Acc const& __acc=_Acc(), const _Cmp& __cmp=_Cmp())
-	: _M_acc(__acc), _M_cmp(__cmp)
-      {
-        for (size_t __i = 0; __i != __K; ++__i)
-          {
-             _M_low_bounds[__i] = _M_acc(__V,__i) - __R;
-             _M_high_bounds[__i] = _M_acc(__V,__i) + __R;
-          }
-      }
+                  template <typename Val>
+                     _Region(Val const& __V, subvalue_type const& __R,
+                           _Acc const& __acc=_Acc(), const _Cmp& __cmp=_Cmp())
+                     : _M_acc(__acc), _M_cmp(__cmp)
+                     {
+                        for (size_t __i = 0; __i != __K; ++__i)
+                        {
+                           _M_low_bounds[__i] = _M_acc(__V,__i) - __R;
+                           _M_high_bounds[__i] = _M_acc(__V,__i) + __R;
+                        }
+                     }
 
-      bool
-      intersects_with(_CenterPt const& __THAT) const
-      {
-        for (size_t __i = 0; __i != __K; ++__i)
-          {
-             // does it fall outside the bounds? 
-             // ! low-tolerance <= x <= high+tolerance
-             // ! (low-tol <= x and x <= high+tol)
-             // !low-tol<=x or !x<=high+tol
-             // low-tol>x or x>high+tol
-             // x<low-tol or high+tol<x
-            if (_M_cmp(__THAT.first._M_low_bounds[__i], _M_low_bounds[__i] - __THAT.second)
-             || _M_cmp(_M_high_bounds[__i] + __THAT.second, __THAT.first._M_low_bounds[__i]))
-              return false;
-          }
-        return true;
-      }
+                  bool
+                     intersects_with(_CenterPt const& __THAT) const
+                     {
+                        for (size_t __i = 0; __i != __K; ++__i)
+                        {
+                           // does it fall outside the bounds? 
+                           // ! low-tolerance <= x <= high+tolerance
+                           // ! (low-tol <= x and x <= high+tol)
+                           // !low-tol<=x or !x<=high+tol
+                           // low-tol>x or x>high+tol
+                           // x<low-tol or high+tol<x
+                           if (_M_cmp(__THAT.first._M_low_bounds[__i], _M_low_bounds[__i] - __THAT.second)
+                                 || _M_cmp(_M_high_bounds[__i] + __THAT.second, __THAT.first._M_low_bounds[__i]))
+                              return false;
+                        }
+                        return true;
+                     }
 
-      bool
-      intersects_with(_Region const& __THAT) const
-      {
-        for (size_t __i = 0; __i != __K; ++__i)
-          {
-            if (_M_cmp(__THAT._M_high_bounds[__i], _M_low_bounds[__i])
-             || _M_cmp(_M_high_bounds[__i], __THAT._M_low_bounds[__i]))
-              return false;
-          }
-        return true;
-      }
-/*
-      bool
-      encloses(value_type const& __V) const
-      {
-        for (size_t __i = 0; __i != __K; ++__i)
-          {
-            if (_M_cmp(_M_acc(__V, __i), _M_low_bounds[__i])
-             || _M_cmp(_M_high_bounds[__i], _M_acc(__V, __i)))
-              return false;
-          }
-        return true;
-      }
-*/
+                  bool
+                     intersects_with(_Region const& __THAT) const
+                     {
+                        for (size_t __i = 0; __i != __K; ++__i)
+                        {
+                           if (_M_cmp(__THAT._M_high_bounds[__i], _M_low_bounds[__i])
+                                 || _M_cmp(_M_high_bounds[__i], __THAT._M_low_bounds[__i]))
+                              return false;
+                        }
+                        return true;
+                     }
 
-	//added by ning to calculate cartesian distance
-	//is the point V within range of this region?
-	bool 
-	encloses(value_type const& __V) const
-	{
-		double distance=0.0;
-		for(size_t __i = 0; __i != __K; ++__i)
-		{
-			double temp;
-			temp=((double)_M_acc(__V, __i)-(double((_M_low_bounds[__i]+_M_high_bounds[__i])))/2.0);
-			distance+=temp*temp;
-		}
-		if(distance>((double)(_M_high_bounds[0]-_M_low_bounds[0])/2.0))
-			return false;
-		return true;
-	}
+                  bool
+                     encloses(value_type const& __V) const
+                     {
+                        for (size_t __i = 0; __i != __K; ++__i)
+                        {
+                           if (_M_cmp(_M_acc(__V, __i), _M_low_bounds[__i])
+                                 || _M_cmp(_M_high_bounds[__i], _M_acc(__V, __i)))
+                              return false;
+                        }
+                        return true;
+                     }
 
 
-	//end by ning
+                  _Region&
+                     set_high_bound(value_type const& __V, size_t const __L)
+                     {
+                        _M_high_bounds[__L % __K] = _M_acc(__V, __L % __K);
+                        return *this;
+                     }
 
-      _Region&
-      set_high_bound(value_type const& __V, size_t const __L)
-      {
-        _M_high_bounds[__L % __K] = _M_acc(__V, __L % __K);
-        return *this;
-      }
+                  _Region&
+                     set_low_bound(value_type const& __V, size_t const __L)
+                     {
+                        _M_low_bounds[__L % __K] = _M_acc(__V, __L % __K);
+                        return *this;
+                     }
 
-      _Region&
-      set_low_bound(value_type const& __V, size_t const __L)
-      {
-        _M_low_bounds[__L % __K] = _M_acc(__V, __L % __K);
-        return *this;
-      }
-
-      subvalue_type _M_low_bounds[__K], _M_high_bounds[__K];
-      _Acc _M_acc;
-      _Cmp _M_cmp;
-    };
+                  subvalue_type _M_low_bounds[__K], _M_high_bounds[__K];
+                  _Acc _M_acc;
+                  _Cmp _M_cmp;
+               };
 
 } // namespace KDTree
 
